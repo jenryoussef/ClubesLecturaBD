31c4d1aae6ebe55138b6034bfaed3a264e300135
diegoprietoucab <daprieto.24@ucab.est.edu.ve>
2026-05-31 23:09:43 -0400

    Conversión ahora bidireccional

--- Conversion_monetaria.sql
+++ Conversion_monetaria.sql
@@ -1,10 +1,17 @@
 CREATE OR REPLACE FUNCTION Conversion_monetaria(
     monto IN NUMBER,                               
-    tasa IN NUMBER       --tasa local/USD              
+    tasa IN NUMBER,       --tasa local/USD    
+    sentido VARCHAR2 DEFAULT 'LOC/USD'
 ) RETURN NUMBER IS
 BEGIN
-    RETURN (monto / tasa);
+    IF sentido = 'LOC/USD' THEN
+        RETURN(monto / tasa);
+    ELSIF sentido = 'USD/LOC' THEN
+        RETURN(monto * tasa);
+    ELSE
+        RAISE_APPLICATION_ERROR(-20001, 'ERROR. El sentido sólo puede ser "LOC/USD" o "USD/LOC"');
+    END IF;
 END Conversion_monetaria;
 /
 
-SELECT Conversion_monetaria(&monto, &tasa_local_a_usd) from dual;
+SELECT Conversion_monetaria(&monto, &tasa_local_a_usd) "Conersión monetaria" from dual;
