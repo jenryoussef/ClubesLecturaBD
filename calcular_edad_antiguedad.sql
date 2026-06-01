47536e4337856b3cdaf9513746fd4bee0acd248f
Prieto Varela, Diego <daprieto.24@est.ucab.edu.ve>
2026-05-31 23:34:10 -0400

    Change age calculation to use MONTHS_BETWEEN

--- calcular_edad_antiguedad.sql
+++ calcular_edad_antiguedad.sql
@@ -3,6 +3,7 @@ CREATE OR REPLACE FUNCTION calcular_edad_antiguedad(
     fecha2 IN DATE DEFAULT SYSDATE
 ) RETURN NUMBER IS
 BEGIN
-    RETURN FLOOR((fecha2 - fecha1) / 365);
+    RETURN TRUNC(MONTHS_BETWEEN(fecha2, fecha1) / 12);
 END calcular_edad_antiguedad;
+/
 /
