2d67354fc7433ca2b71e36366f7ee899b495c382
diegoprietoucab <daprieto.24@ucab.est.edu.ve>
2026-06-13 14:49:19 -0400

    reporte corregido

--- pruebas/prueba_inscripciones.sql
+++ pruebas/prueba_inscripciones.sql
@@ -5,5 +5,8 @@ II) Tamano_Grupos_Disponibles (elección de club)
 III) Miembros_Grupos (ver cierres (splits) y aperturas de hist_asignaciones). Preferiblemente filtrar por club y tipo de grupo
 */
 
+--Inscripción normal
 Execute adfj_inscribir_lector(&id_lector, &id_club);
+
+--Inscripción con split
 Execute adfj_inscribir_lector(&id_lector, &id_club, &dia, TO_DATE('&hora_militar', 'HH24:MI'));
